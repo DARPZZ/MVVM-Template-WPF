6201e4946688f96bedcec8d2921e69250ce2009f
Rasmus <rasmustofthermansen@gmail.com>
2023-09-22 19:14:58 +0200

    INotifyPropertyChanged made

---
+++ MVVM-Template WPF/Helpers/BaseInotify.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Template_WPF.Helpers
+{
+    public class BaseInotify : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnProppertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
